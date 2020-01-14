f6784cd795f7866a7161e5dda271fc409c971ddd
e <1605552882@qq.com>
2020-01-14 11:09:45 +0800

    姜森焱

---
+++ shop/src/test/java/com/shop/domian/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.domian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
